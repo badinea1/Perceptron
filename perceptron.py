788bceb72cdf0841cd588339498a3552d40043b6
vpatel2018 <43823608+vpatel2018@users.noreply.github.com>
2021-05-02 12:49:38 -0400

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -2,24 +2,6 @@ import math
 
 #TODO: test the code
 
-def updatePerceptronWeights(learningRate, weights, trainingEx, targetOutput):
-            
-    perceptronOutput = calcPredictedVal(weights, trainingEx)
-    copyOfTrainingEx = [1]
-    
-    for x in trainingEx:
-        copyOfTrainingEx.append(x)   
-    #
-            
-    for x in range(0, len(weights)):
-        deltaWeight = copyOfTrainingEx[x] * (targetOutput - perceptronOutput) * learningRate
-        weights[x] = weights[x] + deltaWeight
-    #
-  
-    return weights
-    
-#
-
 def calcPredictedVal(weights, trainingEx):
     
     copyOfTrainingEx = [1]
@@ -39,14 +21,25 @@ def calcPredictedVal(weights, trainingEx):
 
 #
 
-def learnFromDataSet(trainingSet, learningRate):
-
-    numOfWeights = len(trainingSet[0])
-    weights = []
+def updatePerceptronWeights(learningRate, weights, trainingEx, targetOutput):
+            
+    perceptronOutput = calcPredictedVal(weights, trainingEx)
+    copyOfTrainingEx = [1]
     
-    for x in range(0, numOfWeights):
-        weights.append(0)                   
+    for x in trainingEx:
+        copyOfTrainingEx.append(x)   
     #
+            
+    for x in range(0, len(weights)):
+        deltaWeight = copyOfTrainingEx[x] * (targetOutput - perceptronOutput) * learningRate
+        weights[x] = weights[x] + deltaWeight
+    #
+  
+    return weights
+    
+#
+
+def learnFromDataSet(trainingSet, learningRate, weights):
             
     for item in trainingSet:
         trainingEx = item[0: len(item) - 1]
@@ -55,5 +48,23 @@ def learnFromDataSet(trainingSet, learningRate):
     #
             
     return weights
+
+#
+
+def getWeightsFromDataSet(trainingSet, learningRate):
+
+    weights = []
+    
+    for x in range(0, len(trainingSet[0])):
+        weights.append(0)             
+    #
+            
+    while(True):
+        newWeights = learnFromDataSet(trainingSet, learningRate, weights)
+        if(newWeights == weights):
+           return weights               
+        #
+        weights = newWeights               
+    #
 
 #
