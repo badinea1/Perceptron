d5ac78fb482adea49961821f975a0794e7c88ff2
vpatel2018 <43823608+vpatel2018@users.noreply.github.com>
2021-05-02 00:00:56 -0400

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -2,7 +2,7 @@ import math
 
 def updatePerceptronWeights(learningRate, weights, trainingEx, perceptronOutput, targetOutput):
     
-    trainingEx.insert(0, 0)
+    trainingEx.insert(0, 1)
         
     for x in range(0, len(weights)):
         deltaWeight = trainingEx[x] * (targetOutput - perceptronOutput) * learningRate
