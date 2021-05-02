af3d82ca8324dcf5bde457cd945213e32d63410f
vpatel2018 <43823608+vpatel2018@users.noreply.github.com>
2021-05-02 00:21:48 -0400

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,9 +1,9 @@
 import math
 
+#TODO: test the code
+
 def updatePerceptronWeights(learningRate, weights, trainingEx, perceptronOutput, targetOutput):
-    
-    trainingEx.insert(0, 1)
-        
+            
     for x in range(0, len(weights)):
         deltaWeight = trainingEx[x] * (targetOutput - perceptronOutput) * learningRate
         weights[x] = weights[x] + deltaWeight
@@ -11,5 +11,24 @@ def updatePerceptronWeights(learningRate, weights, trainingEx, perceptronOutput,
   
     return weights
     
+#
+
+def calcPredictedVal(weights, trainingEx):
+    
+    copyOfTrainingEx = [1]
+    
+    for x in trainingEx:
+        copyOfTrainingEx.append(x)   
+    #
+
+    sum = 0
+        
+    for x in range(0, len(weights)):
+        product = weights[x] * copyOfTrainingEx[x]
+        sum += product
+    #
+    
+    return sum
+
 #
 
