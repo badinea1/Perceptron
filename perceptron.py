a4733f98bc423ecaff103254f4e1eb294423dee6
vpatel2018 <43823608+vpatel2018@users.noreply.github.com>
2021-05-02 18:30:28 -0400

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,9 +1,14 @@
 import math
 
 #NOTE: if there are bugs, which I don't believe there might be, just let me know
-#TODO: write comments for the code
 
-def calcPredictedVal(weights, trainingEx):
+#used to get a perceptron output for a training example
+def getPerceptronOutput(percWeights, trainingEx):
+    
+    '''
+    input: percWeights is a list of weights for a perceptron, trainingEx is a training example
+    output: perceptron output for training example
+    '''
     
     copyOfTrainingEx = [1]
     
@@ -13,47 +18,64 @@ def calcPredictedVal(weights, trainingEx):
 
     total = 0
         
-    for x in range(0, len(weights)):
-        product = weights[x] * copyOfTrainingEx[x]
-        total += product
+    for x in range(0, len(percWeights)):
+        product = percWeights[x] * copyOfTrainingEx[x]
+        total = total + product
     #
     
     return total
 
 #
 
-def updatePerceptronWeights(learningRate, weights, trainingEx, targetOutput):
+#updates weights for a perceptron if it incorrectly classifies a training example
+def updatePerceptronWeights(learningRate, percWeights, trainingEx, targetOutput):
+    
+    '''
+    input: learningRate is a float, percWeights is a list of weights for a perceptron, 
+    trainingEx is a training example, targetOutput is the expected perceptron output for
+    a training example
+    output: a list containing updated weights for a perceptron
+    '''
             
-    perceptronOutput = calcPredictedVal(weights, trainingEx)
+    perceptronOutput = getPerceptronOutput(percWeights, trainingEx)
     copyOfTrainingEx = [1]
     
     for x in trainingEx:
         copyOfTrainingEx.append(x)   
     #
-            
-    for x in range(0, len(weights)):
+    
+    for x in range(0, len(percWeights)):
         deltaWeight = copyOfTrainingEx[x] * (targetOutput - perceptronOutput) * learningRate
-        weights[x] = weights[x] + deltaWeight
+        percWeights[x] = percWeights[x] + deltaWeight
     #
   
-    return weights
+    return percWeights
     
 #
 
-def performOneEpoch(learningRate, weights, trainingSet):
+#performs an epoch of learning
+def performOneEpoch(learningRate, percWeights, trainingSet):
+    
+    '''
+    input: learningRate is a float, percWeights is a list of weights for a perceptron, trainingSet is a list of training examples
+    output: returns [newWeights, numOfErrors], where newWeights is a list that results from updating percWeights during an epoch
+    of learning and numOfErrors is the # of training examples the perceptron incorrectly classifies
+    '''
     
     numOfErrors = 0
      
     for item in trainingSet:
         trainingEx = item[0: len(item) - 1]
         targetOutput = item[len(item) - 1]
-        newWeights = updatePerceptronWeights(learningRate, weights, trainingEx, targetOutput)
-        if(newWeights != weights):
+        weights = updatePerceptronWeights(learningRate, percWeights, trainingEx, targetOutput)
+        if(weights != percWeights):
            numOfErrors = numOfErrors + 1
         #
-        weights = newWeights
+        percWeights = weights
     #
     
-    return [weights, numOfErrors]
+    newWeights = percWeights
+    
+    return [newWeights, numOfErrors]
     
 #
