8cee75cbbd10776d2dd2b9857b36925e8b82aceb
vpatel2018 <43823608+vpatel2018@users.noreply.github.com>
2021-05-01 21:34:41 -0400

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,1 +1,12 @@
+import math
 
+def updatePerceptronWeights(learningRate, weights, trainingEx, perceptronOutput, targetOutput):
+  
+    difference = targetOutput - perceptronOutput
+    product = difference * learningRate
+    
+    
+  
+    pass
+  
+#
