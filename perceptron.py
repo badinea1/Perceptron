080a55ddf802c79db641ebc095298ac91847cd8d
Gordon Petry <gpetry37@gmail.com>
2021-05-03 12:21:07 -0400

    Fixed bugs with the perceptron algorithm and cleaned up documentation

--- perceptron.py
+++ perceptron.py
@@ -1,124 +1,109 @@
 import math
 
-#NOTE: THIS FILE MAY HAVE BUGS, I MIGHT NEED TO FIX THEM
-
-#used to get a perceptron output for a training example
 def getPerceptronOutput(percWeights, trainingEx):
     
     '''
-    input: percWeights is a list of weights for a perceptron, trainingEx is a training example
-    output: perceptron output for training example
+    Used to get a perceptron output for a training example
+
+    Arguments:
+        percWeights : [weight] : List of weights for a perceptron
+        trainingEx : [x] : List of examples in a training example
+
+    Returns: 
+        Perceptron output for training example
     '''
-    
-    copyOfTrainingEx = [1]
-    
-    for x in trainingEx:
-        copyOfTrainingEx.append(x)   
-    #
 
     total = 0
         
     for x in range(0, len(percWeights)):
-        product = percWeights[x] * copyOfTrainingEx[x]
+        product = percWeights[x] * trainingEx[x]
         total = total + product
     #
     
-    if(total > 0):
-       return 1
-    #
-    
-    return -1
+    return total
 
 #
 
-#updates weights for a perceptron if it incorrectly classifies a training example
 def updatePerceptronWeights(learningRate, percWeights, trainingEx, targetOutput):
     
     '''
-    input: learningRate is a float, percWeights is a list of weights for a perceptron, 
-    trainingEx is a training example, targetOutput is the expected perceptron output for
-    a training example
-    output: a list containing updated weights for a perceptron
+    Updates weights for a perceptron if it incorrectly classifies a training example
+
+    Arguments:
+        learningRate : float
+        percWeights : [weight] : list of weights for a perceptron, 
+        trainingEx : [x] : List of examples in a training example
+        targetOutput : float : The expected perceptron output for a training example
+
+    Returns:
+        A list containing updated weights for a perceptron
     '''
             
     perceptronOutput = getPerceptronOutput(percWeights, trainingEx)
-    copyOfTrainingEx = [1]
     
-    for x in trainingEx:
-        copyOfTrainingEx.append(x)   
-    #
-    
-    copyOfPercWeights = []
-    
-    for x in percWeights:
-        copyOfPercWeights.append(x)   
-    #
-    
-    for x in range(0, len(copyOfPercWeights)):
-        deltaWeight = copyOfTrainingEx[x] * (targetOutput - perceptronOutput) * learningRate
-        copyOfPercWeights[x] = copyOfPercWeights[x] + deltaWeight
+    for x in range(0, len(percWeights)):
+        deltaWeight = trainingEx[x] * (targetOutput - perceptronOutput) * learningRate
+        percWeights[x] = percWeights[x] + deltaWeight
     #
   
-    return copyOfPercWeights
+    return percWeights
     
 #
 
-#performs an epoch of learning
-def performOneEpoch(learningRate, percWeights, trainingSet):
+def performOneEpoch(learningRate, percWeights, trainingEx, targetOutput):
     
     '''
-    input: learningRate is a float, percWeights is a list of weights for a perceptron, trainingSet contains training data
-    output: returns [newWeights, numOfErrors], where newWeights is a list that results from updating percWeights during an epoch
-    of learning and numOfErrors is the # of training examples the perceptron incorrectly classifies
+    Performs an epoch of learning
+
+    Arguments:
+        learningRate : float
+        percWeights : [weight] : list of weights for a perceptron
+        trainingEx : [[item,],] : contains training data
+        targetOutput : int
+
+    Returns: [newWeights, numOfErrors] : 
+        newWeights : [weight] : a list that results from updating percWeights during an epoch of learning
+        numOfErrors : # of training examples the perceptron incorrectly classifies
     '''
     
     numOfErrors = 0
-    
-    '''
-    Format of each element in trainingSet: [x_1, ..., x_n, targetOutput]
-    -> targetOutput is a 1 or -1
-    -> x_1 to x_n are used by a perceptron to calculate a # that will be compared with targetOutput when updating perceptron weights
-    '''
-    
-    copyOfPercWeights = []
-    
-    for x in percWeights:
-        copyOfPercWeights.append(x)   
-    #
      
-    for item in trainingSet:
-        trainingEx = item[0: len(item) - 1]
-        targetOutput = item[len(item) - 1]
-        weights = updatePerceptronWeights(learningRate, copyOfPercWeights, trainingEx, targetOutput)
-        if(copyOfPercWeights != weights):
-            numOfErrors = numOfErrors + 1   
+    for example in trainingEx:
+        weights = updatePerceptronWeights(learningRate, percWeights, example, targetOutput)
+        if(weights != percWeights):
+           numOfErrors = numOfErrors + 1
         #
-        copyOfPercWeights = weights
+        percWeights = weights
     #
     
-    newWeights = copyOfPercWeights
+    newWeights = percWeights
     
     return [newWeights, numOfErrors]
     
 #
 
-#records information about every epoch of learning necessary for a training set
-def learnFromDataSet(learningRate, percWeights, trainingSet):
+def learnFromDataSet(learningRate, percWeights, trainingEx, targetOutput):
     
     '''
-    input: learningRate is a float, percWeights is a list of weights for a perceptron,
-    trainingSet contains training data
-    output: a list in which the kth element will contain information about the kth
-    epoch of learning which takes place
+    Records information about every epoch of learning necessary for a training set
+
+    Arguments:
+        learningRate : float
+        percWeights : [weights] : a list of weights for a perceptron,
+        trainingEx : [[item,],] : contains training data
+        targetOutput : int
+    Returns:
+        a list in which the kth element will contain information about the kth
+        epoch of learning which takes place
     '''
  
-    result = performOneEpoch(learningRate, percWeights, trainingSet)
+    result = performOneEpoch(learningRate, percWeights, trainingEx, targetOutput)
     array = [result]
     percWeights = result[0]
     numOfErrors = result[1]
    
     while(True):
-        newResult = performOneEpoch(learningRate, percWeights, trainingSet)
+        newResult = performOneEpoch(learningRate, percWeights, trainingEx, targetOutput)
         newWeights = newResult[0]
         newNumOfErrors = newResult[1]
         if(newNumOfErrors == numOfErrors):
--- project4.py
+++ project4.py
@@ -9,12 +9,14 @@ Run the program using this file
 
 import csv
 import sys
-import perceptron
-import experiment
+
+from perceptron import *
+from experiment import *
 
 def process_file(path):
     
     """
+    Takes in a data file and processes data into a set
 
     Notes:
         Assumes the label for the data lies in the final column
@@ -24,34 +26,38 @@ def process_file(path):
         path : str : relative or absolute path to a csv file
 
     Returns:
-        rows : a list of rows of a csv file
+        processed_data : ({label_i : [data_i]}) : a key-value dictionary,
+            data contains a list of doubles
     """
 
-    rows = []
+    processed_data = dict()
 
     with open(path, newline='') as csvfile:
-        
         for row in csvfile:
             # Remove any newline characters
             row = row.rstrip()
-            
-            if(len(row) > 0):
-                # Split row of file on ',' characters
-                row = row.split(',')
-                
-                rows.append(row)
-            #
-            
-         #
-        
-    #
-    
-    return rows
+            # Split file on ',' characters
+            row = row.split(',')
+            # Remove the label from the data
+            label = row.pop()
+            # Convert list items to floats
+            row = list(map(float, row))
+            # Either append data to existing data list or
+            if label in processed_data:
+                processed_data[label].append(row)
+            # Add a new key to the dictionary and append data to list
+            else:
+                # Ensure the row isn't empty
+                if label != '':
+                    processed_data[label] = list()
+                    processed_data[label].append(row)
+
+    return processed_data
 
 #
 
 def main():
-    
+   
     # Check arguments and assign path to variable
     if len(sys.argv) < 2:
         print("Usage: main.py [INPUT FILE PATH]")
@@ -62,8 +68,19 @@ def main():
     
     # Obtain rows of file specified by sys.argv[1]
     processed_data = process_file(input_path)
+
+    for key in processed_data:
+        print(key)
+        print(processed_data[key])
     
+    # Set values for use
+    learningRate = .01
+    percWeights = [.1,.4,.8,.2]
+    trainingEx = processed_data["Iris-virginica"]
+    targetOutput = 1
+
     # Call perceptron algorithm
+    print(learnFromDataSet(learningRate, percWeights, trainingEx, targetOutput))
 
 if __name__ == "__main__":
     
