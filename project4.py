254226e3cf6fa2a86c8b6847714ec555c8fc34a5
vpatel2018 <43823608+vpatel2018@users.noreply.github.com>
2021-05-03 17:00:16 -0400

    Update project4.py

--- project4.py
+++ project4.py
@@ -95,17 +95,73 @@ def getTrainingDataForLP(label, processed_data):
 def main():
    
     # Check arguments and assign path to variable
-    if len(sys.argv) < 2:
-        print("Usage: main.py [INPUT FILE PATH]")
+    if len(sys.argv) < 4:
+        print("Usage: project4.py [INPUT FILE PATH] [TASK NUMBER] [LEARNING RATE]")
         sys.exit()
     #
     
+    # sys.argv[1] has path to .data file
     input_path = sys.argv[1]
     
-    # Obtain rows of file specified by sys.argv[1]
+    # sys.argv[2] is task number
+    taskNum = float(sys.argv[2])
+    
+    # sys.argv[3] is learning rate
+    learningRate = float(sys.argv[3])
+    
+    # possibleTasks is an array of task numbers 
+    possibleTasks = [2, 3.1, 3.2, 3.3, 4.1, 4.2]
+    
+    index = -1
+    
+    for x in range(0, len(possibleTasks)):
+        if(possibleTasks[x] == taskNum):
+            index = x
+            break
+        #
+    #
+    
+    if(index == -1):
+        print("Possible Task Numbers -> {2, 3.1, 3.2, 3.3, 4.1, 4.2}")
+        print("You entered an invalid task number")
+    #
+    
+    # process data from a .data file provided by input_path
     processed_data = process_file(input_path)
+    
+    '''
+    if you want to do task 4, you would have to shuffle data from .data file
+    provided by input_path
+    '''
+    
+    if((taskNum == 4.1) | (taskNum == 4.2)):
+        taskNum = 4
+        experiment.shuffleData(input_path)
+        processed_data = process_file('shuffledData.data')
+        os.system('rm shuffledData.data')
+    #
 
-    #write additional code
+    # Create initial weights based on task number
+    sizeOfWeightVector = len(processed_data[list(processed_data)[0]][0]) + 1
+    percWeights = experiment.getInitWeightVector(sizeOfWeightVector, taskNum)
+    
+    # Create training data for LP 1
+    trainingSet1 = getTrainingDataForLP('Iris-setosa', processed_data)
+    
+    # Create training data for LP 2
+    trainingSet2 = getTrainingDataForLP('Iris-versicolor', processed_data)
+    
+    # Create training data for LP 3
+    trainingSet3 = getTrainingDataForLP('Iris-virginica', processed_data)
+    
+    # get information about every epoch of learning for trainingSet1
+    array1 = perceptron.learnFromDataSet(learningRate, percWeights, trainingSet1)
+    
+    # get information about every epoch of learning for trainingSet2
+    array2 = perceptron.learnFromDataSet(learningRate, percWeights, trainingSet2)
+    
+    # get information about every epoch of learning for trainingSet3
+    array3 = perceptron.learnFromDataSet(learningRate, percWeights, trainingSet3)
 
 if __name__ == "__main__":
     
