c13e99f2b7a8f2f0e4ba832fe17e1d4304f69e5f
vpatel2018 <43823608+vpatel2018@users.noreply.github.com>
2021-05-06 00:08:40 -0400

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -11,26 +11,26 @@ Amulya Badineni
 Yuriy Deyneka
 '''
 
-#initializes a weight vector for tasks 2, 3.1, 3.2, 3.3, 4.1, 4.2
+#initializes a weight vector for tasks 2, 3.1, 3.2, 3.3, 4 
 def getInitWeightVector(sizeOfWeightVector, taskNum):
   
     '''
-    input: sizeOfWeightVector represents the size of weight vector that needs to be initialized, taskNum represents a task number
-    output: a list of weights
+    input: sizeOfWeightVector represents the size of the weight vector that needs to be initialized, taskNum represents a task number 
+    output: a list of weights 
     '''
     
     array = []
     taskNum = float(taskNum)
     
-    #for tasks 2 and 4, create a weight vector that has 0s ONLY
+    #for tasks 2 and 4, create a weight vector that has 0s ONLY 
     if((taskNum == 2) | (taskNum == 4)):
        array = [0] * sizeOfWeightVector
                
-    #for task 3.1, create a weight vector that has 1s ONLY
+    #for task 3.1, create a weight vector that has 1s ONLY 
     elif(taskNum == 3.1):
        array = [1] * sizeOfWeightVector
         
-    #for tasks 3.2 and 3.3, create a weight vector that has decimals greater than 0 and less than 1
+    #for tasks 3.2 and 3.3, create a weight vector that has decimals greater than 0 and less than 1 
     elif((taskNum == 3.2) | (taskNum == 3.3)):
        for x in range(0, sizeOfWeightVector):
            number = random.random()
@@ -42,25 +42,21 @@ def getInitWeightVector(sizeOfWeightVector, taskNum):
     
 #
 
-#shuffles data in some .data file in random order
+#shuffles data in some .data file in random order 
 def shuffleData(fileName):
-  
+    
     '''
-    input: absolute or relative path to a .data file
-    output: a .data file called shuffledData.data
+    input: fileName is a string that represents an absolute or relative path to a .data file 
+    output: a .data file called shuffledData.data 
     '''
     
     rows = []
   
     with open(fileName, newline='') as csvfile:
         for row in csvfile:
-            # Remove any newline characters
             row = row.rstrip()
-            
             if(len(row) > 0):
-                # Split file on ',' characters
                 row = row.split(',')
-                
                 rows.append(row)    
             #
         #
